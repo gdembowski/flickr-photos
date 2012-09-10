1cef0d5c2e7c7e9641b8f9ca9675b109d278c26e
Grz <wladimirec@gmail.com>
2012-09-10 22:52:45 +0200

    * added Favourite button using localStorage

--- photos.js
+++ photos.js
@@ -5,6 +5,7 @@ var setupPhotos = (function ($) {
         var i;
         for (i = 0; i < items.length; i += 1) {
             setTimeout(callback.bind(this, items[i]), 0);
+            //callback.call(this, items[i]);
         }
     }
 
@@ -64,9 +65,19 @@ var setupPhotos = (function ($) {
     function imageAppender (id) {
         var holder = document.getElementById(id);
         return function (img) {
+		
+			
             var elm = document.createElement('div');
+			var heart = document.createElement('i');
+			if (checkFav(img.src) !== false) {
+				heart.className = 'icon-heart';
+			} else {
+				heart.className = 'icon-heart-empty';
+			}
+			
             elm.className = 'photo';
             elm.appendChild(img);
+			elm.appendChild(heart);
             holder.appendChild(elm);
         };
     }
@@ -83,3 +94,110 @@ var setupPhotos = (function ($) {
         });
     };
 }(jQuery));
+
+// -------
+
+function checkFav(imglink) {
+	var favs = [];
+	if(typeof(Storage)!=="undefined") {
+		if (localStorage.getItem('favLinks') && localStorage.getItem('favLinks').length > 0) {
+			favs = JSON.parse(localStorage.getItem('favLinks'));
+		}
+		
+	}
+	
+	
+	if (favs !== null && favs.length > 0) {
+		// search in array
+		if (favs.indexOf(imglink) != -1) {
+			return favs.indexOf(imglink);
+		}
+	}
+	return false;
+}
+
+
+function clickFav(imglink, callback) {
+	
+	var check = checkFav(imglink);
+	
+	if(typeof(Storage) !== "undefined") {
+		// SessionStorage
+		var stored = [];
+		if (localStorage.getItem('favLinks') && localStorage.getItem('favLinks').length > 0) {
+			stored = JSON.parse(localStorage.getItem('favLinks'));
+		}
+		
+		if (check === false) {
+			// Not found - insert
+			if (stored !== null && stored.length > 0) {
+				stored.push(imglink);
+			} else {
+				stored = [imglink];
+			}
+		} else {
+			// Found - delete
+			if (stored !== null && stored.length > 0) {
+				stored.splice(check, 1);
+			}
+		}
+		// SetData
+		try {
+			localStorage.setItem('favLinks', JSON.stringify(stored));
+		} catch(e) {
+			localStorage.removeItem('favLinks');
+			alert("Storage error");
+		}
+	} else {
+		console.error("Update your broswer");
+	}
+	
+	
+	// Visual work
+	callback(check);
+}
+
+/*
+
+ jQuery
+ 
+$('div.photo i').live('click', function() {
+	var link = $(this).prev('img').attr('src');
+	elem = $(this);
+
+	clickFav(link, function(state) {
+		elem.attr('class', '');
+
+		if (state === false) {
+			// Add to Favs
+			elem.attr('class', 'icon-heart');
+		} else {
+			// Remove from Favs
+			elem.attr('class', 'icon-heart-empty');
+		}
+	
+	});
+
+});
+*/
+
+
+document.onclick = function(event) {
+    el = event.target;
+    if (el.nodeName == "I") {
+		var link = el.parentNode.firstChild.getAttribute("src");
+    
+		clickFav(link, function(state) {
+			el.removeAttribute('class');
+
+			if (state === false) {
+				// Add to Favs
+				el.setAttribute('class', 'icon-heart');
+			} else {
+				// Remove from Favs
+				el.setAttribute('class', 'icon-heart-empty');
+			}
+		
+		});
+    }
+};
